84301dafac12213e390da565ec8c97fe46e02f7c
nikita <example@gmail.com>
2020-09-04 00:54:24 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,14 @@
+// alert('Hello');
+// const result = confirm('Are you here?');
+// console.log(result);
+
+// const answer = prompt('Вам есть 18?', '18');
+// console.log(answer);
+// const age = +prompt('What is ur age');
+// alert(age + 6);
+
+const category = 'toys';
+
+alert(`hello/${category}`);
+alert(8 == '8');
+alert(8 === '8');
